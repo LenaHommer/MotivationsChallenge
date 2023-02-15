4bfca054eeb2f09f797e3260db9842ce9b454d1a
LenaHommer <mariamagdalena.hommer@iu-study.org>
2023-02-15 16:08:33 +0100

    erste Schritte

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to MotivationChallange"/>
+  <div>
+    <Login></Login>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "./components/Login.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Login,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+@import "~bootstrap/dist/css/bootstrap.min.css";
+@import "~@fortawesome/fontawesome-free/css/all.min.css";
+
 </style>
---
+++ src/components/Login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="login">
+    <h1>Login</h1>
+    <div class="form-inputs">
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username" v-model="input.username" placeholder="Username" />
+        </div>
+        <div class="form-inputs">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" v-model="input.password" placeholder="Password" />
+        </div>
+        <button type="button" v-on:click="login()">Login</button>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data() {
+        return {
+            input: {
+                username: "",
+                password:""
+            }
+        }
+    },
+    methods: {
+        login() {
+            
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
