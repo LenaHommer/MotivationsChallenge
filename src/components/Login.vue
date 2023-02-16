a9d8ecd8731e72a3489df69e45343eb5e1c6ee1e
LenaHommer <mariamagdalena.hommer@iu-study.org>
2023-02-16 11:36:41 +0100

    Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,17 +1,40 @@
 <template>
-    <div id="login">
-    <h1>Login</h1>
-    <div class="form-inputs">
-            <label for="username">Username</label>
-            <input type="text" id="username" name="username" v-model="input.username" placeholder="Username" />
+ <div id="login">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-5 offset-xl-4">
+                <div class="card mt-5">
+                    <div class="card-header">
+                        <img src="../assets/friends-g7ecf65acf_1920.jpg" class="img-fluid">
+                        <h1 class="text-center">Anmelden</h1>
+                            <div class="card-body">
+                                    <div class="form-floating">
+                                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                        <label for="floatingInput">Email address</label>
+                                    </div>
+                                     <div class="form-floating">
+                                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                        <label for="floatingPassword">Password</label>
+                                    </div>
+                                <button class="w-100 btn btn-lg btn-primary" type="button" v-on:click="login()">Login</button>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="form-inputs">
-            <label for="password">Password</label>
-            <input type="password" id="password" name="password" v-model="input.password" placeholder="Password" />
-        </div>
-        <button type="button" v-on:click="login()">Login</button>
-        </div>
+    </div>
+ </div>
 </template>
+
+/*   <div class="form-inputs">
+        <input type="text" id="username" name="username" v-model="input.username" placeholder="Username" />
+        <label for="username">Username/Email address</label>
+    </div>
+    <div class="form-inputs">
+        
+        <input type="password" id="password" name="password" v-model="input.password" placeholder="Password" />
+        <label for="password">Password</label>
+    </div> */
 
 <script>
 export default {
