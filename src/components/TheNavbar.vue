e0231d35d75690b16e8d3741e958deed69c82ce4
LenaHommer <mariamagdalena.hommer@iu-study.org>
2023-03-22 21:10:23 +0100

    Navbar collapse

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,6 +1,11 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-vue navbar-dark">
+<!--   <nav class="navbar navbar-expand-lg bg-vue navbar-dark">
     <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+        <div class="mobile-nav-button"><span>&equiv;</span></div>
+
         <router-link to="/first" class="navbar-brand">mydescipline</router-link>
         <ul class="navbar-nav me-auto">
             <li class="nav-item active">
@@ -13,8 +18,41 @@
         <button class="btn bg-vue2" @click="signout()">
             <i class="fas fa-sign-out-alt"></i> Logout
         </button>
+
+            </div>
+        </div>
     </div>
-  </nav>
+  </nav> -->
+
+
+<nav class="navbar navbar-expand-lg bg-vue navbar-dark">
+  <div class="container-fluid mt-2">
+    <a class="navbar-brand" href="#">mydiscipline</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarText"
+      aria-controls="navbarText" 
+      aria-expanded="false" 
+      aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item active">
+          <router-link to="/first" class="nav-link" href="#">Home<span class="sr-only">(current)</span></router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/create/challenge" class="nav-link" href="#">new Challenge</router-link>
+        </li>
+      </ul>
+      <button class="btn bg-vue2" @click="signout()">
+            <i class="fas fa-sign-out-alt"></i> Logout
+    </button>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script>
@@ -34,4 +72,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
